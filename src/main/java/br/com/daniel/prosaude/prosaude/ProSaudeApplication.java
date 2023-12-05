7cd573057ad5dc6188d1eb69147d47b6a906e54e
Daniel Teixeira <daniellhrt@hotmail.com>
2023-12-05 15:14:10 -0300

    Commit inicial

---
+++ src/main/java/br/com/daniel/prosaude/prosaude/ProSaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.prosaude.prosaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daniel/prosaude/prosaude/ProsaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.prosaude.prosaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
